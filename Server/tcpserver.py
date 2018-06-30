a5b8a388b47a32ae9c7a9a0f6e62409d85adbef1
theselfengineer <theselfengineer@gmail.com>
2018-06-30 18:28:58 +0200

    fucking leds

--- Server/tcpserver.py
+++ Server/tcpserver.py
@@ -4,14 +4,14 @@ import threading
 from commands import Commands
 from controller import Controller
 
-from main import Main.on_moved
+from main import Main
 
 
 class TcpServer(threading.Thread):
     """ Simple socket server that listens to one single client. """
 
     c = Controller()
-    c.moved.connect(on_moved)
+    c.moved.connect(Main.on_moved)
 
     def __init__(self, host = '0.0.0.0', port = 12345, buf_size = 1024):
         threading.Thread.__init__(self)
