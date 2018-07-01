e0f8b05f7a2607d83523d50fdf3ccff92aa269da
theselfengineer <theselfengineer@gmail.com>
2018-07-01 08:13:14 +0200

    arduino

--- Server/arduino.py
+++ Server/arduino.py
@@ -5,7 +5,7 @@ import threading
 import sys
 
 class Arduino(threading.Thread):
-    def __init__(self, text, out):
+    def __init__(self):
         threading.Thread.__init__(self)
         self.ser = serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
         self.ser.baudrate=9600
